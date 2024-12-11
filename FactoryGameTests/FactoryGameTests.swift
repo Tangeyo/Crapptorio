dc244ca663160b103969aaf5b91a0b975eaf500e
Tangeyo <82197718+Tangeyo@users.noreply.github.com>
2024-12-11 11:50:38 -0800

    Add additional files for xcode

---
+++ FactoryGameTests/FactoryGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryGameTests.swift
+//  FactoryGameTests
+//
+//  Created by Ava Albert on 11/12/24.
+//
+
+import Testing
+@testable import FactoryGame
+
+struct FactoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
